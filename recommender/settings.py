b15a54ed5aca2bb7d95c4cc07459cc2e82e9dd4f
gigabyte <yumayuda249@gmail.com>
2025-01-01 15:29:39 +0700

    home link

--- recommender/settings.py
+++ recommender/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j@7@z=25eihdqck8wpuiz%t_h^ez+l4i8_87fqlrah+9vc(auq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
